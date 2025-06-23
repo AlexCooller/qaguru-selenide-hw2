3e3fb223d793e47d964e25c1eeaef4261a43ead3
AlexCooller <al.kulakov@bk.ru>
2025-06-23 15:44:59 +0300

    add test Github to enterprize

---
+++ src/test/java/GithubSolutions.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubSolutions {
+
+    @BeforeAll
+    static void onSettings() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void solutionsHoverEnterprize(){
+        open("/");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $("ul.list-style-none").shouldBe(visible).$(byText("Enterprises")).hover().click();
+        $("h1").$(byText("The AI-powered" + "developer platform"));
+        sleep(5000);
+    }
+}
